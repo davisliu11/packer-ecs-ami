e3f0872b3ef3865c205401f95c14eb2b32d8f297
slacksec <idthuang@gmail.com>
2017-02-09 00:21:12 +1300

    Create 08-docker-storeage-setup.sh

---
+++ ecs_scripts/08-docker-storeage-setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# This is just a test to make sure cloud-init is picking this up
+( echo -n "# Output generated on " ; date )|tee /var/tmp/cloudinit.out
+echo "Running cloud-init..." |tee -a /var/tmp/cloudinit.out
+
+# Stopping services
+stop ecs
+service docker stop
+
+
+# Deleting disk partition
+fdisk /dev/xvdcz <<EOF
+d
+w
+EOF
+
+# Remove cloud-init sempahore files
+rm -rf /var/lib/cloud/sem/*
+
+# Remove docker storage settings
+rm -rf /var/lib/docker/*
+
+# Hack docker-storage-setup script
+if [ ! `grep -q "\-ff" /usr/bin/docker-storage-setup` ]; then
+   sed -i 's#pvcreate ${dev}1#pvcreate -ff -y ${dev}1#g' /usr/bin/docker-storage-setup
+fi
